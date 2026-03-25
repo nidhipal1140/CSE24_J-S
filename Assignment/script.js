f77da6ea54994cb94e786e4698dcdb845df0eb1d
Nidhi Pal <palnidhi061@gmail.com>
2026-03-25 18:07:51 +0530

    Assignment

---
+++ Assignment/script.js
@@ -0,0 +1,15 @@
+
+let result = a+b;
+console.log('a+b= ',result);
+
+result = a-b;
+console.log('a-b= ',result);
+
+result = a*b;
+console.log('a*b= ',result);
+
+result = a/b;
+console.log('a/b= ',result);
+
+result = a%b;
+console.log('a%b= ',result);
